8d882ca3252157eb0827f30a33eabd68c694670b
Vladimir6332 <vladimir6332@gmail.com>
2020-10-21 15:58:35 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let res = n.toString().split("").reverse();
+    if (res[res.length - 1] === "-") {
+        res.pop();
+        res = res.join("");
+    } else res = res.join("");
+    return res;
+};
